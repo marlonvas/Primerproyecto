71e5fcab2feeae59b0c62ba099ecd506bd265932
Marlon <mdvasquezss@gmail.com>
2022-08-30 22:22:28 -0500

    mi primer commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const user ={
+    name: "John",
+    email: "john@gmail.com",
+    password: "123456",
+    role: "admin",
+    avatar: "https://avatars0.githubusercontent.com/u/1256?v=4",
+    status: "active",
+    adress: {
+        email: "john@gmail.com",
+        password: "123456",
+        calle:  "John",
+    }
+
+     
+}
+
+console.log(user.adress.email);
